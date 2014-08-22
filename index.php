02e842de6a476292a0925510c1bc0802b65da887
Max Verbeek <m4xv3rb33k@gmail.com>
2014-08-22 15:15:32 +0200

    Modified example

--- index.php
+++ index.php
@@ -2,12 +2,19 @@
 
 include 'Rooster.php';
 
-$rooster = new Rooster(13899, "sA4", 962);
+if (isset($_GET['student'], $_GET['class'], $_GET['school']))
+{
+	if (is_int($_GET['student']) && is_int($_GET['school']))
+	{
+		$rooster = new Rooster($_GET['student'], $_GET['class'], $_GET['school']);
+		$table = $rooster->getVertTable();
+	}
+}
 
-$table = $rooster->getVertTable();
+if (!isset($rooster)) $leerlingen = include("Leerlingen.php");
+
 
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -15,38 +22,57 @@ $table = $rooster->getVertTable();
 	<title>Rooster shit</title>
 </head>
 <body>
-	<table>
-		<tr>
-			<th>Uur</th>
-			<th>Maandag</th>
-			<th>Dinsdag</th>
-			<th>Woensdag</th>
-			<th>Donderdag</th>
-			<th>Vrijdag</th>
-		</tr>
+	<?php if (isset($rooster)): ?>
+		<table>
+			<tr>
+				<th>Uur</th>
+				<th>Maandag</th>
+				<th>Dinsdag</th>
+				<th>Woensdag</th>
+				<th>Donderdag</th>
+				<th>Vrijdag</th>
+			</tr>
 
-		<?php
+			<?php
 
-		foreach ($table as $uur => $row)
-		{
-			$uur++;
-			echo "<tr>";
-
-			echo "<td>" . $uur . "</td>";
-
-			foreach ($row as $day)
+			foreach ($table as $uur => $row)
 			{
-				echo "<td>";
-				foreach ($day as $item => $value)
-				{
-					echo "{$item}: $value<br>";
-				}
-				echo "</td>";
-			}
+				$uur++;
+				echo "<tr>";
 
-			echo "</tr>";
-		}
-		?>
-	</table>
+				echo "<td>" . $uur . "</td>";
+
+				foreach ($row as $day)
+				{
+					echo "<td>";
+					foreach ($day as $item => $value)
+					{
+						echo "{$item}: $value<br>";
+					}
+					echo "</td>";
+				}
+
+				echo "</tr>";
+			}
+			?>
+		</table>
+	<?php else:
+		// display selector for students
+	?>
+	<form action="#" method="GET">
+		<div class="field"><select name="student">
+			<?php foreach ($leerlingen as $class => $leerling): ?>
+				<optgroup label="<?php echo $class; ?>">
+					<?php foreach ($leerling as $id => $individu): ?>
+						<option value="<?php echo $id; ?>"><?php echo $individu; ?></option>
+					<?php endforeach; ?>
+				</optgroup>
+			<?php endforeach; ?>
+		</select></div>
+		<div class="field"><input type="submit"></div>
+	</form>
+
+	<?php endif; ?>
+
 </body>
 </html>
